b89e3f93970cfda0024a4597664c2fd9d4dfb23e
Pavel Kulakovsky <pavelkulakovsky@MacBook-Pro-2.local>
2021-01-02 00:43:42 +0200

    version 1.0.0 added

---
+++ precache-manifest.e2a0b0f709cf57d491b85950f1d6afd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e631760bdcdf93d8776d",
+    "url": "/static/js/main.d9dfb6f2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ca8d4e5f9fd2331d65ef",
+    "url": "/static/css/2.c0eb4bef.chunk.css"
+  },
+  {
+    "revision": "ca8d4e5f9fd2331d65ef",
+    "url": "/static/js/2.8f6918b7.chunk.js"
+  },
+  {
+    "revision": "93902b4051380c9abb3c5bce062cd322",
+    "url": "/index.html"
+  }
+];
